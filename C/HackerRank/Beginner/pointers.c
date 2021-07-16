1582bf378a48bda19ff34806627fffe5be2ea351
mrdrzit <uzunaruto.10.mat@gmail.com>
2021-07-15 23:35:15 -0300

    Put function on the end

--- C/HackerRank/Beginner/pointers.c
+++ C/HackerRank/Beginner/pointers.c
@@ -1,14 +1,11 @@
 // Complete the function void update(int *a,int *b). 
 // It receives two integer pointers, int* a and int* b. 
-// et the value of a to their sum, and b to their absolute difference. 
+// Set the value of a to their sum, and b to their absolute difference. 
 // There is no return value, and no return statement is needed.
-
 #include <stdio.h>
+#include <stdlib.h>
 
-void update(int *a,int *b) 
-{
-            
-}
+void update(int *a, int *b);
 
 int main() {
     int a, b;
@@ -19,4 +16,12 @@ int main() {
     printf("%d\n%d", a, b);
 
     return 0;
+}
+
+void update(int *a, int *b) 
+{
+    int tmp_a = *a;
+    int tmp_b = *b;
+    *a = tmp_a + tmp_b;
+    *b = ((abs(tmp_a)) - (abs(tmp_b)));
 }
