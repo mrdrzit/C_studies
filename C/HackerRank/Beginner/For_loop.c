37b9ada48fbce36d5609ca3464488577344b07e2
mrdrzit <uzunaruto.10.mat@gmail.com>
2021-07-16 11:16:56 -0300

    For loop with conditions

--- C/HackerRank/Beginner/For_loop.c
+++ C/HackerRank/Beginner/For_loop.c
@@ -1,24 +1,35 @@
-//Task For each integer n in the interval [a, b] (given as input) : 
-// • If 1 < n < 9, then print the English representation of it in lowercase. That is "one" for 1, "two" for 2, and so on. 
-// • Else if n > 9 and it is an even number, then print "even". 
-// • Else if n > 9 and it is an odd number, then print "odd". 
+//Task For each integer n in the interval [a, b] (given as input) :
+// • If 1 < n < 9, then print the English representation of it in lowercase. That is "one" for 1, "two" for 2, and so on.
+// • Else if n > 9 and it is an even number, then print "even".
+// • Else if n > 9 and it is an odd number, then print "odd".
 
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
 
-
-
-int main() 
+int main()
 {
-    char *words[9] = {"one","three", "four", "five", "six", "seven", "eight", "nine"};
     int a, b;
+    printf("numeros ");
     scanf("%d\n%d", &a, &b);
-  	
-    for(int i = a; i < b; i++)
-    {
-    }
+    char *words[9] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 
-    return 0;
-}
+        for (int i = a; i <= b; i++)
+        {
+            int currenti = i;
+            if (i >= 1 && i <= 9)
+            {
+                printf("%s\n", words[i - 1]);
+            }
+            else if (i > 9 && (i % 2 == 0))
+            {
+                printf("even\n");
+            }
+            else if (i > 9 && (i % 2 != 0))
+            {
+                printf("odd\n");
+            }
+        }
+        return 0;
+    }
