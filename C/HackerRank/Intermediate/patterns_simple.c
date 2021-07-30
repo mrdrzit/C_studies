581ec44f1ef37be29324826f3074044162436408
mrdrzit <uzunaruto.10.mat@gmail.com>
2021-07-30 16:40:50 -0300

    Added dynamic array to "create array" function

--- C/HackerRank/Intermediate/patterns_simple.c
+++ C/HackerRank/Intermediate/patterns_simple.c
@@ -5,19 +5,38 @@
 
 // Print a pattern of numbers from 1 to n as shown below. Each of the numbers is separated by a single space.
 
-//                             4 4 4 4
-//                             4 3 3 3
-//                             4 3 2 2
-//                             4 3 2 1
+//                             4 4 4 4 4 4 4
+//                             4 3 3 3 3 3 4
+//                             4 3 2 2 2 3 4
+//                             4 3 2 1 2 3 4
+//                             4 3 2 2 2 3 4
+//                             4 3 3 3 3 3 4
+//                             4 4 4 4 4 4 4
 
-void print_line(int array[], int line_size, int subtract, int which)
+//                                4 4 4 4
+//                                4 3 3 3
+//                                4 3 2 2
+//                                4 3 2 1
+
+int *create_array(int numero_de_elementos, int n)
+{
+    int *array = malloc(numero_de_elementos * sizeof(int));
+
+    for (int i = 0; i < numero_de_elementos; i++)
+    {
+        array[i] = n;
+    }
+    return array;
+}
+
+void print_line(int *array, int line_size, int subtract, int which, int stop, int n)
 {
 
     for (int i = 0; i < line_size; i++)
     {
-        if (i >= which)
+        if (i >= which && i < stop)
         {
-            array[i] = line_size - subtract;
+            array[i] = n - subtract;
         }
         printf("%d ", array[i]);
     }
@@ -26,22 +45,26 @@ void print_line(int array[], int line_size, int subtract, int which)
 
 int main()
 {
-    int subtract = 0; //How much do i need to subtract
-    int which = 0; //at which element do i need to start subtracting
+    int subtract = 0; // How much do i need to subtract
+    int which = 0; // At which element do i need to start subtracting
 
-    const int n = 9;
+    const int n = 4;
+    const int num_colunas = (n * 2) - 1;
 
-    int line[] = {[0 ... 8] = n};
+    int *line = create_array(num_colunas, n);
+    int stop = num_colunas; // At wich element do i need to stop subtracting
 
     for (int iline = 0; iline < n; iline++)
     {
-        print_line(line, n, subtract, iline);
+        print_line(line, num_colunas, subtract, which, stop, n);
         subtract++;
         which++;
+        stop--;
         if (iline == n - 1)
         {
             printf("\n");
         }
     }
+    free(line);
     return 0;
 }
