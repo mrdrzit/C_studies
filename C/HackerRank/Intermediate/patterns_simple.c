fcc5b7b0300669d0eaa962b1e43230b4232668c1
mrdrzit <uzunaruto.10.mat@gmail.com>
2021-07-30 15:30:53 -0300

    Create loop to print patterns based on input

---
+++ C/HackerRank/Intermediate/patterns_simple.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+// Print a pattern of numbers from 1 to n as shown below. Each of the numbers is separated by a single space.
+
+//                             4 4 4 4
+//                             4 3 3 3
+//                             4 3 2 2
+//                             4 3 2 1
+
+void print_line(int array[], int line_size, int subtract, int which)
+{
+
+    for (int i = 0; i < line_size; i++)
+    {
+        if (i >= which)
+        {
+            array[i] = line_size - subtract;
+        }
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int subtract = 0; //How much do i need to subtract
+    int which = 0; //at which element do i need to start subtracting
+
+    const int n = 4;
+
+    int line[] = {[0 ... 3] = n};
+    int printing = n;
+
+    for (int iline = 0; iline < n; iline++)
+    {
+        int num_print = (n - iline);
+        which = iline;
+
+        print_line(line, n, subtract, which);
+        subtract++;
+        which++;
+        if (iline == n - 1)
+        {
+            printf("\n");
+        }
+    }
+    return 0;
+}
