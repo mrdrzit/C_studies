f3bd0f130c6b935f364abea1e00536feac25724c
mrdrzit <uzunaruto.10.mat@gmail.com>
2021-07-30 10:04:42 -0300

    First file of the intermediate branch

---
+++ C/HackerRank/Intermediate/patterns.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+// Print a pattern of numbers from 1 to n as shown below. Each of the numbers is separated by a single space.
+
+//                             4 4 4 4 4 4 4  
+//                             4 3 3 3 3 3 4   
+//                             4 3 2 2 2 3 4   
+//                             4 3 2 1 2 3 4   
+//                             4 3 2 2 2 3 4   
+//                             4 3 3 3 3 3 4   
+//                             4 4 4 4 4 4 4
+
+int main() 
+{
+
+    int n;
+    scanf("%d", &n);
+  	// Complete the code to print the pattern.
+    return 0;
+}
