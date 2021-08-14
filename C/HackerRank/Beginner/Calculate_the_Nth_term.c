7124f26dc4631d55976b3b95545255baf288de1a
mrdrzit <uzunaruto.10.mat@gmail.com>
2021-08-14 12:28:53 -0300

    Comments added

--- C/HackerRank/Beginner/Calculate_the_Nth_term.c
+++ C/HackerRank/Beginner/Calculate_the_Nth_term.c
@@ -9,19 +9,19 @@ int sum = 0;
 int find_nth_term(int n, int a, int b, int c)
 {
     int array[3]; array[0] = a; array[1] = b; array[2] = c;
-    if (n <= 3)
+    if (n <= 3) //condição pra checar se tão pedindo algum número que não precisa somar (i.e. um dos 3 primeiros)
     {
         return array[n-1];
     }
 
     sum = a + b + c;
 
-    if (0 >= (n - 3))
+    if (0 >= (n - 3)) // Se menor do que n-3 pra excluir os valores que não foram somados (i.e. um dos 3 primeiros)
     {
         return sum;
         
     }
-    n--;
+    n--; // usa o alvo como um índice máximo de iteração, diminuindo a cada chamada recursiva
 
     int actual = sum + find_nth_term(n, b, c, sum);
     return sum;
