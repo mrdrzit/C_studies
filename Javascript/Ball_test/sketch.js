2134ca1d714e9800c69c602bf44f3e6e4e876a4b
mrdrzit <uzunaruto.10.mat@gmail.com>
2021-12-19 14:21:13 -0300

    Create a bouncing ball using a class

--- Javascript/Ball_test/sketch.js
+++ Javascript/Ball_test/sketch.js
@@ -1,30 +1,36 @@
 /// <reference path="TSDef/p5.global-mode.d.ts" />
 "use strict";
 
-var gravity = 1;
-var y = 0;
+let ball;
 
-function setup(){
+function setup() {
   createCanvas(400, 400);
+  ball = new Ball(30);
 }
 
-function draw(){
-  background(100);
-  fill(210,120,22);
-  noStroke();
-  ellipseMode(CENTER);
-  ellipse(width/2, y, 30);
+function draw() {
+  background(0);
+  ball.show();
+  ball.move();
+}
 
-  if (y > height || y < 0){
-    gravity = gravity * -1;
+class Ball {
+  constructor(size) {
+    this.x = 200;
+    this.y = 200;
+    this.diameter = size;
+    this.speed = 2;
   }
-  y = y + gravity
-}
 
-class ball{
-  constructor(){
-    this.x = width/2;
-    this.y = height/2;
-    let velocity = 0;
+  show() {
+    circle(this.x, this.y, this.diameter)
+  }
+
+  move() {
+    this.x = this.x + this.speed;
+    if (this.x > width || this.x < 0){
+      this.speed = this.speed * -1;
+      fill(random(20,255), random(50,255), random(80,255))
+    }
   }
 }
