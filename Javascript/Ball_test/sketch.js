e511fb602306131192c4790e259c2d93819ebdfb
mrdrzit <uzunaruto.10.mat@gmail.com>
2021-12-20 13:06:56 -0300

    Added vector to store objects and some angle funcs

--- Javascript/Ball_test/sketch.js
+++ Javascript/Ball_test/sketch.js
@@ -1,34 +1,41 @@
 /// <reference path="TSDef/p5.global-mode.d.ts" />
 "use strict";
 
-let ball;
+
+let balls = [];
+
 
 function setup() {
   createCanvas(400, 400);
-  ball = new Ball(30);
+  for (let i = 0; i < 1; i++){
+    balls[i] = new Ball(random(5,40));
+    angleMode(DEGREES);
+  }
 }
 
 function draw() {
   background(0);
-  ball.show();
-  ball.move();
+  for (let i = 0; i < balls.length; i++){
+    balls[i].show();
+    balls[i].move();
+  }
 }
 
 class Ball {
   constructor(size) {
-    this.x = 200;
-    this.y = 200;
+    this.pos = createVector(200,200)
     this.diameter = size;
     this.speed = 2;
+    this.heading = this.pos.heading(random(0,TWO_PI));
   }
 
   show() {
-    circle(this.x, this.y, this.diameter)
+    circle(this.pos.x, this.pos.y, this.diameter)
   }
 
   move() {
-    this.x = this.x + this.speed;
-    if (this.x > width || this.x < 0){
+    this.pos.x = this.pos.x + this.speed;
+    if (this.pos.x > width || this.pos.x < 0){
       this.speed = this.speed * -1;
       fill(random(20,255), random(50,255), random(80,255))
     }
