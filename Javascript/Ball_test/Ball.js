cefd2191be93aabfa6f1e4f330d9e6b5c5848bbd
mrdrzit <uzunaruto.10.mat@gmail.com>
2021-12-21 17:32:27 -0300

    Fomatting

--- Javascript/Ball_test/Ball.js
+++ Javascript/Ball_test/Ball.js
@@ -1,45 +1,45 @@
 class Ball {
-    constructor(size, color = 120) {
-        this.pos = createVector(random(90, height - 100), random(80, height - 100));
-        this.speed = p5.Vector.random2D().mult(3);
-        this.diameter = size;
-        this.color = color;
-    }
+  constructor(size, color = 120) {
+    this.pos = createVector(random(90, height - 100), random(80, height - 100));
+    this.speed = p5.Vector.random2D().mult(3);
+    this.diameter = size;
+    this.color = color;
+  }
 
-    isOverlapping(object) {
-        let d = dist(this.pos.x, this.pos.y, object.pos.x, object.pos.y);
-        if (d < this.diameter/2 + object.diameter/2) {
-            return true;
-        }else {
-            return false;
-        }
+  isOverlapping(object) {
+    let d = dist(this.pos.x, this.pos.y, object.pos.x, object.pos.y);
+    if (d < this.diameter / 2 + object.diameter / 2) {
+      return true;
+    } else {
+      return false;
     }
+  }
 
-    changeColor(lambda){
-        this.color = lambda;
+  changeColor(lambda) {
+    this.color = lambda;
+  }
+
+  isInside(xpos, ypos) {
+    if (dist(xpos, ypos, this.pos.x, this.pos.y) > this.diameter / 2) {
+      return false;
+    } else {
+      return true;
     }
+  }
 
-    isInside(xpos, ypos) {
-        if (dist(xpos, ypos, this.pos.x, this.pos.y) > this.diameter / 2) {
-            return false;
-        } else {
-            return true;
-        }
+  show() {
+    fill(this.color)
+    circle(this.pos.x, this.pos.y, this.diameter)
+  }
+
+  move() {
+    this.pos.add(this.speed);
+
+    if (this.pos.x > width || this.pos.x < 0) {
+      this.speed.x = this.speed.x * -1;
     }
-
-    show() {
-        fill(this.color)
-        circle(this.pos.x, this.pos.y, this.diameter)
+    else if (this.pos.y > height || this.pos.y < 0) {
+      this.speed.y = this.speed.y * -1;
     }
-
-    move() {
-        this.pos.add(this.speed);
-
-        if (this.pos.x > width || this.pos.x < 0) {
-            this.speed.x = this.speed.x * -1;  
-        }
-        else if (this.pos.y > height || this.pos.y < 0) {
-            this.speed.y = this.speed.y * -1;
-        }
-    }
+  }
 }
