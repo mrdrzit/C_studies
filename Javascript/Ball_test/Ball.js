eb7433a3e69d490bfa8243f9d0a0da9763d896a8
mrdrzit <uzunaruto.10.mat@gmail.com>
2021-12-21 13:37:10 -0300

    New file for Ball class and checking boundaries

---
+++ Javascript/Ball_test/Ball.js
@@ -0,0 +1,34 @@
+class Ball {
+    constructor(size) {
+        this.pos = createVector(random(20, height - 20), random(20, height - 20));
+        this.speed = p5.Vector.random2D().mult(2);
+        this.diameter = size;
+        this.color = color(255);
+    }
+
+    show() {
+        fill(this.color)
+        circle(this.pos.x, this.pos.y, this.diameter)
+    }
+
+    isInside(xpos, ypos) {
+        if (dist(xpos, ypos, this.pos.x, this.pos.y) > diameter / 2) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    move() {
+        this.pos.add(this.speed);
+
+        if (this.pos.x > width || this.pos.x < 0) {
+            this.speed.x = this.speed.x * -1;
+            this.color = color(random(20, 255), random(50, 255), random(80, 255));
+        }
+        else if (this.pos.y > height || this.pos.y < 0) {
+            this.speed.y = this.speed.y * -1;
+            this.color = color(random(20, 255), random(50, 255), random(80, 255));
+        }
+    }
+}
--- Javascript/Ball_test/sketch.js
+++ Javascript/Ball_test/sketch.js
@@ -7,6 +7,10 @@ let balls = [];
 function setup() {
   createCanvas(400, 400);
   angleMode(DEGREES);
+  for (let i = 0; i < 5; i++){
+    let ball = new Ball(50);
+    balls.push(ball);
+  }
 }
 
 function draw() {
@@ -17,34 +21,10 @@ function draw() {
   }
 }
 
-function mousePressed(){
-  let ball = new Ball(mouseX,mouseY,random(20,45));
-  balls.push(ball);
-}
-
-class Ball {
-  constructor(xpos, ypos, size) {
-    this.pos = createVector(xpos, ypos, 200);
-    this.speed = p5.Vector.random2D().mult(3);
-    this.diameter = size;
-    this.color = color(255);
-  }
-
-  show() {
-    fill(this.color)
-    circle(this.pos.x, this.pos.y, this.diameter)
-  }
-
-  move() {
-    this.pos.add(this.speed);
-
-    if (this.pos.x > width || this.pos.x < 0) {
-      this.speed.x = this.speed.x * -1;
-      this.color = color(random(20,255), random(50,255), random(80,255));
-    }
-    else if (this.pos.y > height || this.pos.y < 0) {
-      this.speed.y = this.speed.y * -1;
-      this.color = color(random(20,255), random(50,255), random(80,255));
+function mousePressed() {
+  for (let i = 0; i < balls.length; i++){
+    if (balls[i].isInside()){
+      return
     }
   }
 }
