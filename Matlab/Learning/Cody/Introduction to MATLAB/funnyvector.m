bbb97b37f47c44012b51c6718b8a4f051311e5f7
mrdrzit <uzunaruto.10.mat@gmail.com>
2021-12-23 20:19:00 -0300

    Add prelloc to speed up program

--- Matlab/Learning/Cody/Introduction to MATLAB/funnyvector.m
+++ Matlab/Learning/Cody/Introduction to MATLAB/funnyvector.m
@@ -1,9 +1,11 @@
 function vector = funnyvector(n)
     %Generate a vector like 1,2,2,3,3,3,4,4,4,4
-    vector = [];
+    vector = zeros(sum(1:n),1);
+    i=1;
     for isize = 1:n
         for irepeat = 1:isize
-            vector(end + 1) = isize;
+            vector(i) = isize;
+            i=i+1;
         end
     end
 end
