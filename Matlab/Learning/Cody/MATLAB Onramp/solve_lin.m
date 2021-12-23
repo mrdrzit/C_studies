71f76edd757c1a326eff02fb11c4abc836520813
mrdrzit <uzunaruto.10.mat@gmail.com>
2021-12-22 23:41:04 -0300

    Create solve_lin file

---
+++ Matlab/Learning/Cody/MATLAB Onramp/solve_lin.m
@@ -0,0 +1,8 @@
+function x = solve_lin(theta)
+% Given a constant input angle θ (theta) in radians, create the coefficient matrix (A) and constant vector (b) to solve the given
+% system of linear equations in x₁ and x₂.
+% cos(θ) x₁ + sin(θ) x₂ = 1
+% -sin(θ) x₁ + cos(θ) x₂ = 1
+
+
+%TODO: #3 Implement solution using mldivide
