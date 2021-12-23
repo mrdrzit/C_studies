3f6bc2f272be987b1323e48ec1aadd48e745d0f8
mrdrzit <uzunaruto.10.mat@gmail.com>
2021-12-22 21:31:53 -0300

    Failed to implement DFT

---
+++ Python/Fourier_try/DTFT.py
@@ -0,0 +1,4 @@
+# Fórmula ---> X(k) = sum_{t=0}^{n-1} x(t) e^{-2 π i t k / n}
+
+import matplotlib as plt
+import numpy as np
--- Python/Fourier_try/Fourier.py
+++ Python/Fourier_try/Fourier.py
@@ -3,10 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Fórmula >>> y(t) = Asin(2PI * f * t + PHI)
-Fs = 8000;    # Amostragem
+Fs = 1000;    # Amostragem
 dt = 1/Fs;    # seconds per sample
 Time = 3;     # seconds
 timeVector = np.linspace(0, Time, int(Fs*Time), endpoint=False)
+freqvector = np.arange(0,10,1)
 
 def createWave(A, F, T):
     y = A * np.sin(2 * np.pi * F * timeVector)
@@ -17,14 +18,11 @@ b = createWave(2,4,timeVector)
 c = createWave(5,6,timeVector)
 d = np.add(a,b)
 
-plt.subplot(4,1,1)
+plt.subplot(5,1,1)
 plt.plot(timeVector,a)
-plt.subplot(4,1,2)
+plt.subplot(5,1,2)
 plt.plot(timeVector,b)
-plt.subplot(4,1,3)
+plt.subplot(5,1,3)
 plt.plot(timeVector,c)
-plt.subplot(4,1,4)
+plt.subplot(5,1,4)
 plt.plot(timeVector,d)
-
-plt.show()
-
