e7b6dce9ef2e2193319fc40f7396b45e40410245
mrdrzit <uzunaruto.10.mat@gmail.com>
2021-12-22 22:58:39 -0300

    Stupid mistake on cake_frosting

--- Matlab/Learning/Cody/MATLAB Onramp/cake_frosting.m
+++ Matlab/Learning/Cody/MATLAB Onramp/cake_frosting.m
@@ -1,9 +1,6 @@
-function [SA,SA2] = cake_frosting(r, h)
-fprintf('Radius is %i\nHeight is %i',r,h)
+function [SA2] = cake_frosting(r, h)
+%fprintf('Radius is %i\nHeight is %i\n',r,h)
 
-%EDIT LATER
-
-SA = 2*pi*r*(h + r);
-SA2 = (2*pi*r*h)+(2*pi*r*r);
+SA2 = (2*pi*r*h)+(pi*r*r);
 
 end
