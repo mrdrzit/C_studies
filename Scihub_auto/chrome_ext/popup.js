91d86e5874382da13a5d479b26ea5a0468d9a9f9
mrdrzit <uzunaruto.10.mat@gmail.com>
2022-03-02 12:44:21 -0300

    Formatting

--- Scihub_auto/chrome_ext/popup.js
+++ Scihub_auto/chrome_ext/popup.js
@@ -6,24 +6,24 @@ chrome.storage.sync.get("color", ({ color }) => {
 
 // When the button is clicked, inject setPageBackgroundColor into current page
 changeColor.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: fuction_alert,
-    });
-  });
-  
-  // The body of this function will be executed as a content script inside the
-  // current page
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-  function fuction_alert(){
-    links = document.links;
-    for (var i = 0; i < links.length; i++){
-      let tmp = links[i].toString()
-      if (tmp.includes("doi.org/")){
-        let store_link = links[i].getAttribute("href");
-        console.log(store_link);
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: fuction_alert,
+  });
+});
+
+// The body of this function will be executed as a content script inside the
+// current page
+
+function fuction_alert() {
+  links = document.links;
+  for (var i = 0; i < links.length; i++) {
+    let tmp = links[i].toString()
+    if (tmp.includes("doi.org/")) {
+      let store_link = links[i].getAttribute("href");
+      console.log(store_link);
       let newURL = "https://sci-hub.se/" + store_link;
       chrome.runtime.sendMessage(newURL);
       break
