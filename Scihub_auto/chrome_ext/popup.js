5e25f097a76a24adcff8525c0ea2013d2a72ebb3
mrdrzit <uzunaruto.10.mat@gmail.com>
2022-03-02 12:44:28 -0300

    Cleanup old code

--- Scihub_auto/chrome_ext/popup.js
+++ Scihub_auto/chrome_ext/popup.js
@@ -30,8 +30,3 @@ function fuction_alert() {
     }
   }
 }
-  chrome.browserAction.onClicked.addListener(function(activeTab)
-{
-    var newURL = "http://www.youtube.com/watch?v=oHg5SJYRHA0";
-    chrome.tabs.create({ url: newURL });
-});
