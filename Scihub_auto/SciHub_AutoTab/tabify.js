0a88e1188043bfcbd74391bd9fe9eb8a16915258
mrdrzit <uzunaruto.10.mat@gmail.com>
2022-03-03 13:23:11 -0300

    Code formatting

--- Scihub_auto/SciHub_AutoTab/tabify.js
+++ Scihub_auto/SciHub_AutoTab/tabify.js
@@ -1,14 +1,13 @@
 var current_domain = window.location.hostname;
 
 if (current_domain != "www.nature.com") {
-links = document.links;
-for (var i = 0; i < links.length; i++) {
-    let tmp = links[i].toString()
-    if (tmp.includes("doi.org/")) {
-    let store_link = links[i].getAttribute("href");
-    console.log(store_link);
-    let newURL = "https://sci-hub.se/" + store_link;
-    chrome.runtime.sendMessage(newURL);
-    break
+    links = document.links;
+    for (var i = 0; i < links.length; i++) {
+        let tmp = links[i].toString()
+        if (tmp.includes("doi.org/")) {
+            let store_link = links[i].getAttribute("href")
+            let newURL = "https://sci-hub.se/" + store_link
+            chrome.runtime.sendMessage(newURL);
+            break
+        }
     }
-}
