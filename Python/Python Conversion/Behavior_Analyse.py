d969af358d8cb3b16c4639f3700e10eab91507cd
mrdrzit <uzunaruto.10.mat@gmail.com>
2022-03-25 23:50:13 -0300

    To dos

--- Python/Python Conversion/Behavior_Analyse.py
+++ Python/Python Conversion/Behavior_Analyse.py
@@ -4,3 +4,7 @@ from tkinter import ttk
 root = Tk()
 root.title("Define Settings")
 
+
+#TODO #5 Create an input dialog to "Define Settings"
+#TODO #6 Import and organize files based on extension
+#TODO #7 Create an imput dialog that defines "Figure Organization"
