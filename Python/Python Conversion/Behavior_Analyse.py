050aa85338b13bacf96918aea6521394641cd803
mrdrzit <uzunaruto.10.mat@gmail.com>
2022-03-26 13:02:04 -0300

    Header Class

--- Python/Python Conversion/Behavior_Analyse.py
+++ Python/Python Conversion/Behavior_Analyse.py
@@ -1,7 +1,17 @@
+from email import header
 from tkinter import *
 from tkinter import ttk
+from tkinter import filedialog as fd
 from Settings_dialog import *
-import numpy as np
+import pandas as pd
+
+class Header:
+  def __init__(self, Last_Frame, File_Info, File_Name_Csv, File_Name_Png):
+    self.Last_Frame = Last_Frame
+    self.File_Info = File_Info
+    self.File_Name_Csv = File_Name_Csv
+    self.File_Name_Png = File_Name_Png
+
 
 create_Settings_dialog()
 
