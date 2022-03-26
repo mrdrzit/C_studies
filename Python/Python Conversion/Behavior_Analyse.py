ed89ec24332e6cac9876685605d01ce3c68ec2bd
mrdrzit <uzunaruto.10.mat@gmail.com>
2022-03-25 23:54:59 -0300

    Fucntion to close the dialog

--- Python/Python Conversion/Behavior_Analyse.py
+++ Python/Python Conversion/Behavior_Analyse.py
@@ -4,6 +4,26 @@ from tkinter import ttk
 root = Tk()
 root.title("Define Settings")
 
+def close_window():
+    global plts, Exp_nm, Exp_tp, Frames, ArenaWidth, ArenaHeight
+    plts = Exp_name.get()
+    Exp_nm = Exp_type.get()
+    Exp_tp = Plots.get()
+    Frames = Fps.get()
+    ArenaWidth = Arena_width.get()
+    ArenaHeight = Arena_height.get()
+    root.destroy()
+
+root = Tk()
+root.title("Define Settings")
+
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe.grid(column=0,row=0, sticky=(N, W, E, S))
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+Btn = ttk.Button(mainframe, text="OK", command = close_window)
+Btn.grid(column=9, row=9, sticky=E)
+
 
 #TODO #5 Create an input dialog to "Define Settings"
 #TODO #6 Import and organize files based on extension
