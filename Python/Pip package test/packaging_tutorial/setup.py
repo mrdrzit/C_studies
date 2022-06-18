89ed167ec11f44a43bcbd579ac7f4ab7f9b76bc9
mrdrzit <uzunaruto.10.mat@gmail.com>
2022-06-18 18:30:12 -0300

    Basic directory structure

---
+++ Python/Pip package test/packaging_tutorial/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Test_package-Mrdrzit",
+    version="0.0.1",
+    author="Matheus C.",
+    author_email="uzunaruto.10.mat@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mrdrzit/Behavython",
+    project_urls={
+        "Bug Tracker": "https://github.com/mrdrzit/Behavython/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU GENERAL PUBLIC LICENSE",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
