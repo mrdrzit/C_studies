4f7b1571dfa59ed9506afdc60802f39535a6cf48
mrdrzit <uzunaruto.10.mat@gmail.com>
2023-01-03 22:00:53 -0300

    Simple challenges from hackerrank

---
+++ Python/HackerRank/Weird.py
@@ -0,0 +1,25 @@
+import math
+import os
+import random
+import re
+import sys
+
+'''
+Given an integer, n, perform the following conditional actions:
+
+If n is odd, print Weird
+If n is even and in the inclusive range of 2 to 5, print Not Weird
+If n is even and in the inclusive range of 6 to 20, print Weird
+If n is even and greater than 20, print Not Weird
+'''
+
+
+if __name__ == '__main__':
+  n = int(input("digite um numero: ").strip())
+  
+  if n % 2 != 0 or (n % 2 == 0 and n in range(6, 21)):
+    print("Weird")
+  elif n % 2 == 0 and n > 20 or (n % 2 == 0 and n in range(2, 6)):
+    print("Not Weird")
+
+
---
+++ Python/HackerRank/leap.py
@@ -0,0 +1,24 @@
+
+'''
+In the Gregorian calendar, three conditions are used to identify leap years:
+
+The year can be evenly divided by 4, is a leap year, unless:
+The year can be evenly divided by 100, it is NOT a leap year, unless:
+The year is also evenly divisible by 400. Then it is a leap year.
+
+'''
+
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+year = int(input("Ano: "))
+print(is_leap(year))
