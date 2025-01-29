1b115392f8c1dc57728eca9f04a0ce675569a624
Matheus <uzunaruto.10.mat@gmail.com>
2025-01-29 18:46:59 -0300

    Add new version of decrapify

--- Script to automatically decrapify nvidia drivers/decrapify.py
+++ Script to automatically decrapify nvidia drivers/decrapify.py
@@ -1,18 +1,16 @@
 import os
 import shutil
 
-default_folders = ["Display.Driver","Display.Nview","Display.Optimus","Display.Update","FrameViewSDK","GFExperience","GFExperience.NvStreamSrv","HDAudio","MSVCRT","nodejs","NvBackend","NvContainer","NVI2","NvModuleTracker","NVPCF","NvTelemetry","NvVAD","NvvHCI","PhysX","PPC","ShadowPlay","ShieldWirelessController","Update.Core","decrapify.py","EULA.txt","ListDevices.txt","setup.cfg","setup.exe"]
-folders = ["Display.Nview","Display.Optimus","Display.Update","FrameViewSDK","GFExperience.NvStreamSrv","nodejs","NvBackend","NvContainer","NvModuleTracker","NVPCF","NvTelemetry","NvVAD","NvvHCI","PPC","ShadowPlay","ShieldWirelessController","Update.Core"]
+cleaned_version = ["Display.Driver", "HDAudio", "MSVCRT", "PhysX", "NvDLISR", "NvApp"]
 
 path = os.getcwd()
 dirs = os.listdir(path)
 
-if len(default_folders) != len(dirs):
-    print("This version of the driver have additional folders, please change the list inside the script")
-    exit()
-
 for folder in dirs:
-    for key in folders:
-        if folder == key:
-            path_current = path+"\\"+key
+    if folder in cleaned_version:
+        continue
+    else:
+        path_current = os.path.abspath(os.path.join(path, folder))
+        if os.path.isdir(path_current):
             shutil.rmtree(path_current, ignore_errors=False, onerror=None)
+            print(f"Deleted {folder}")
